c0b553f8507a58dfdff3bf7ce96557c1a1b0067b
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-31 09:07:28 -0400

    Disable UI on taking snapshot

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,21 @@
 extends Node
 
 func _ready() -> void:
-	%CameraAnimationPlayer.play("take_snapshot")
+	%CameraAnimationPlayer.connect("animation_started", on_camera_animation_started)
 	%CameraAnimationPlayer.connect("animation_finished", on_camera_animation_finished)
-	
+	%CameraAnimationPlayer.play("take_snapshot")
+
+func on_camera_animation_started(anim: String) -> void:
+	if anim == "take_snapshot":
+		%UI.visible = false
+			
 func on_camera_animation_finished(anim: String) -> void:
 	if anim == "take_snapshot":
-		var snapshot = take_snapshot()
-		%UI.set_snapshot(snapshot)
+		%UI.set_snapshot(get_snapshot())
 		%CameraAnimationPlayer.play("return_to_normal")
+	if anim == "return_to_normal":
+		%UI.visible = true
 		
-func take_snapshot() -> Texture2D:
+func get_snapshot() -> Texture2D:
 	var img = %Camera2D.get_viewport().get_texture().get_image()
 	return ImageTexture.create_from_image(img)
