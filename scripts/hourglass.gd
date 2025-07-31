f0cea0d7e448d0487b15bfc20d188d8960a50bd2
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-31 09:51:48 -0400

    Make state names and export variable

--- scripts/hourglass.gd
+++ scripts/hourglass.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
+@export var state_names: Array[StringName]
+
 enum SAND_STATES { TOP_FULL, TOP_HALF, TOP_EMPTY }
 var sand_state = SAND_STATES.TOP_FULL
-var state_names = ["sand_state"]
 
 func get_state() -> Dictionary:
 	var state = {}
 	
 	for state_name in state_names:
-		state[state_name] = get(state_name)
+		var value = get(state_name)
+		if value != null:
+			state[state_name] = value
+		else:
+			printerr('Error: state "' + state_name + '" not found in ' + name)
 	
 	return state
 
