7181ebe07e57a7333fe06910e28a582522fe3ed7
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-31 11:15:28 -0400

    Loop game if not won

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,12 +5,10 @@ var initial_state
 func _ready() -> void:
 	%CameraAnimationPlayer.animation_started.connect(on_camera_animation_started)
 	%CameraAnimationPlayer.animation_finished.connect(on_camera_animation_finished)
-	%CameraAnimationPlayer.play("take_snapshot")
-	%UI.set_time_slider_max_value(%DayTimer.wait_time)
 	%DayTimer.timeout.connect(on_day_timer_timeout)
+	%UI.set_time_slider_max_value(%DayTimer.wait_time)
 	initial_state = %World.get_state()
-	print('World state initialized at:')
-	print(initial_state)
+	%CameraAnimationPlayer.play("take_snapshot")
 	
 func _process(delta: float) -> void:
 	%UI.set_time_slider_value(%DayTimer.wait_time - %DayTimer.time_left)
@@ -22,6 +20,11 @@ func get_snapshot() -> Texture2D:
 func is_same_state(state1, state2) -> bool:
 	return state1 == state2
 	
+func reset_world(state) -> void:
+	initial_state = state
+	%DayTimer.start()
+	%CameraAnimationPlayer.play("take_snapshot")
+	
 func on_day_timer_timeout() -> void:
 	var final_state = %World.get_state()
 	print()
@@ -30,8 +33,7 @@ func on_day_timer_timeout() -> void:
 	if is_same_state(initial_state, final_state):
 		print('Game won!')
 	else:
-		%World.set_state(final_state)
-		initial_state = final_state
+		reset_world(final_state)
 		print('Loop')
 		
 func on_camera_animation_started(anim: String) -> void:
